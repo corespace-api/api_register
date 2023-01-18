90e6906835b6caae0905c2e294a1857946283621
AsP3X <development@vorberg-webmail.de>
2023-01-18 13:38:01 +0100

    FIX: issue with no respons send when debug disabled

--- service.js
+++ service.js
@@ -82,8 +82,10 @@ service.use((req, res, next) => {
       message: "You are not allowed to access this resource",
       status: 403
     });
+    return;
+  } else {
+    next();
   }
-  return;
 });
 
 apiRoutes.forEach(route => {
